4dc4f684598cad721e3e657d3f4936a1f6f139ef
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2026-04-27 23:56:11 +0300

    updated Entity

--- src/main/java/com/shh/crud/repository/UserRepository.java
+++ src/main/java/com/shh/crud/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.shh.crud.repository;
 
-import com.shh.crud.model.UserEntity;
+import com.shh.crud.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 
 }
